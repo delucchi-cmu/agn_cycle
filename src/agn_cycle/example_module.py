afd8bc02041ef9d3210ce0faaa26bb2876907e68
delucchi-cmu <delucchi@andrew.cmu.edu>
2023-03-23 22:14:12 -0400

    checkpoint

--- src/agn_cycle/example_module.py
+++ src/agn_cycle/example_module.py
@@ -2,12 +2,21 @@
 
 
 import numpy as np
-from photutils.aperture import aperture_photometry
+from photutils.aperture import RectangularAnnulus, aperture_photometry
 from PIL import Image
 
 
-def read_bmp(file_name):
-    img= Image.open(file_name)
+def read_bmp(file_name, centroid):
+    """perform RectAL analysis on AGN jet image."""
+    img = Image.open(file_name)
     np_img = np.array(img)
-    
-    print(np_img.shape)
+
+    # print(np_img.shape)
+
+    aperture_a = RectangularAnnulus(centroid, 25.0, 100.0, 100.0, 25.0)
+    aperture_b = RectangularAnnulus(centroid, 50.0, 150.0, 150.0, 50.0)
+    aperture_c = RectangularAnnulus(centroid, 100.0, 260.0, 260.0, 100.0)
+    phot_table = aperture_photometry(np_img, [aperture_a, aperture_b, aperture_c])
+    print(phot_table)
+
+    return phot_table
